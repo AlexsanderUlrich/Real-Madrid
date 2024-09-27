f87fdcd739d1c443b3ff611d1776ef838e1f962c
Alexsander <desenvolvimentodatachat@gmail.com>
2024-09-27 16:43:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+let botao = document.getElementById('meuBotao');
+let numeroCartao = document.getElementById('numeroCartao');
+let agencia = document.getElementById('agencia');
+let codigo = document.getElementById('codigo');
+
+// Função para verificar se todos os campos estão preenchidos
+function verificarCampos() {
+    if (numeroCartao.value && agencia.value && codigo.value) {
+        botao.disabled = false; // Habilita o botão
+    } else {
+        botao.disabled = true; // Desabilita o botão
+    }
+}
+
+// Adiciona evento de entrada a cada campo
+numeroCartao.addEventListener('input', verificarCampos);
+agencia.addEventListener('input', verificarCampos);
+codigo.addEventListener('input', verificarCampos);
+
+
+botao.addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/resultado.html';
+});
